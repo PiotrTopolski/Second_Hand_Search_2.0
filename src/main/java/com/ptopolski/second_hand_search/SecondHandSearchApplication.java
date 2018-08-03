29c13852ebea7b7cce5401dbfa22d33ea4d2633c
PiotrTopolski <piotr.topolski88@gmail.com>
2018-08-03 23:41:53 +0200

    first commit

---
+++ src/main/java/com/ptopolski/second_hand_search/SecondHandSearchApplication.java
@@ -0,0 +1,12 @@
+package com.ptopolski.second_hand_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandSearchApplication.class, args);
+    }
+}
